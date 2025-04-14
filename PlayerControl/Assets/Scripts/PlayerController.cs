4be1cd7ca05488ef6ed3f35a75db61632c905c40
Sebastian Greg <fortunatos@roverkids.org>
2025-04-14 13:33:26 -0400

    Improves vehicle movement
    Updates vehicle movement to be framerate independent and sets a reasonable speed.

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle forward   
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
