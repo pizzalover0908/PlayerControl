6c18d0b1f8587c1e5bdf502cac3cddf966afecc7
Sebastian Greg <fortunatos@roverkids.org>
2025-04-16 14:13:05 -0400

    Implements basic player movement and camera follow
    Implements basic player movement using horizontal input.
    Adds a camera that follows the player.
    Adds rigidbodies to obstacles.

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // We'll move the vehicle forward   
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate (Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
+
+
+
 }
